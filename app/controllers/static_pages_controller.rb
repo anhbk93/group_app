2d6b718e563b6487c360a58f538f221b1d1dbc59
Phuc <phuc.t.vo.bkdn@gmail.com>
2015-10-21 09:29:31 +0700

    [Add] integration_test following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,10 @@
 # Static Pages Controller
 class StaticPagesController < ApplicationController
   def home
-    return unless logged_in?
-    @micropost  = current_user.microposts.build
-    @feed_items = current_user.feed.paginate(page: params[:page])
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
