c6ef796a56f6199ad854e2207296ea9e1e967000
Phuc <phuc.t.vo.bkdn@gmail.com>
2015-10-21 10:16:33 +0700

    [Modify] Update user_profile_test and Following_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -96,7 +96,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
   end
 
-  test "feed should have the right posts" do
+  test 'feed should have the right posts' do
     michael = users(:michael)
     archer  = users(:archer)
     lana    = users(:lana)
